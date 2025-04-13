edd031cd90ba8ba4ce3246f1ce9f5e44c387c68d
Maher Zubair <za998192@gmail.com>
2025-04-13 23:33:24 +0500

    small change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   tagline: "Documentation & guide for the @nexoracle/utils library. @nexoracle/utils is a Utilities library for JavaScript and TypeScript.",
   favicon: "https://cdn.nexoracle.com/__dirname/images/nexoracle/__icon32.ico",
 
-  url: "https://nexoracle-utils.pages.dev",
+  url: "https://nexoracle-utils.vercel.app",
   baseUrl: "/",
 
   organizationName: "nexoracle",
